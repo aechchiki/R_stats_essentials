6d93831452089470a5debee7812ed103741948c4
aechchiki <a.echchiki@gmail.com>
2017-05-16 09:47:24 +0200

    power of test

---
+++ RstatsEssentials_3_power-of-test.R
@@ -0,0 +1,13 @@
+### power of test
+
+power.t.test()
+	#n= nb d'observations par groupe
+	#delta = écart-type
+	#power= puissance  ~80 (cas standard)
+power.anova.test()
+	#groups = a = df+1
+	#n = (df-a)/a
+	#between.var = msb/n0
+	#within.var = msw
+	#sig.level = niveau de significativité (0.05)
+	#power = 0.8
